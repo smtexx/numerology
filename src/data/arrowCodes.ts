5e2e5b81e7a5db6098ed31ec4eee94e8d7a6dcde
Roman Kolmakov <smtexx@hotmail.com>
2023-01-15 20:49:22 +0100

    Create arrow codes

---
+++ src/data/arrowCodes.ts
@@ -0,0 +1,19 @@
+import { Arrows } from 'types';
+
+export const arrowCodes: { [key in Arrows]: string } = {
+  purposefulness: '048+',
+  procrastination: '048-',
+  spirituality: '246+',
+  research: '246-',
+  intelligence: '258+',
+  forgetfulness: '258-',
+  equilibrium: '147+',
+  hypersensitivity: '147-',
+  practicality: '036+',
+  chaos: '036-',
+  planning: '012+',
+  will: '345+',
+  dissatisfaction: '345-',
+  activity: '678+',
+  passivity: '678-',
+};
