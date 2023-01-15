aaf7e34b0337bb9138b1e0522429797dd892c88a
Roman Kolmakov <smtexx@hotmail.com>
2023-01-15 20:49:55 +0100

    Create getArrows method

--- src/modules/NatalChart.ts
+++ src/modules/NatalChart.ts
@@ -1,4 +1,5 @@
-import { SingleDigit } from 'types';
+import { arrowCodes } from '../data/arrowCodes';
+import { Arrows } from 'types';
 import NumerologyBirthDate from './NumerologyBirthDate';
 
 export default class NatalChart {
@@ -19,5 +20,37 @@ export default class NatalChart {
       }
     });
     return matches;
+  }
+
+  getArrows(): Arrows[] {
+    const arrows: Arrows[] = [];
+
+    Object.entries(arrowCodes).forEach(([arrowName, arrowCode]) => {
+      const code = arrowCode
+        .slice(0, 3)
+        .split('')
+        .map((val) => parseInt(val));
+      const flag = arrowCode.slice(-1);
+
+      if (
+        flag === '+' &&
+        this.natalChart[code[0]] &&
+        this.natalChart[code[1]] &&
+        this.natalChart[code[2]]
+      ) {
+        arrows.push(arrowName as Arrows);
+      }
+
+      if (
+        flag === '-' &&
+        !this.natalChart[code[0]] &&
+        !this.natalChart[code[1]] &&
+        !this.natalChart[code[2]]
+      ) {
+        arrows.push(arrowName as Arrows);
+      }
+    });
+
+    return arrows;
   }
 }
