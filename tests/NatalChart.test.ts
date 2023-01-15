7d9799cb24a6c88b45d43213c4b2de49bccd72fb
Roman Kolmakov <smtexx@hotmail.com>
2023-01-15 20:50:12 +0100

    Testing getArrows method

--- tests/NatalChart.test.ts
+++ tests/NatalChart.test.ts
@@ -23,4 +23,72 @@ describe('NatalChart', () => {
     natalChart = new NatalChart(birthDate);
     expect(natalChart.getMatches()).toEqual(['1111', '999']);
   });
+
+  test('Returns wright arrow for birth date', () => {
+    let birthDate = new NumerologyBirthDate(31, 3, 1950);
+    let natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual(['purposefulness']);
+
+    birthDate = new NumerologyBirthDate(3, 4, 2002);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'procrastination',
+      'passivity',
+    ]);
+
+    birthDate = new NumerologyBirthDate(6, 7, 1953);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'purposefulness',
+      'spirituality',
+      'intelligence',
+    ]);
+
+    birthDate = new NumerologyBirthDate(20, 2, 1981);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'research',
+      'dissatisfaction',
+    ]);
+
+    birthDate = new NumerologyBirthDate(7, 3, 1960);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'intelligence',
+      'hypersensitivity',
+    ]);
+
+    birthDate = new NumerologyBirthDate(15, 8, 2007);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'forgetfulness',
+      'equilibrium',
+    ]);
+
+    birthDate = new NumerologyBirthDate(23, 4, 1987);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'practicality',
+      'planning',
+      'activity',
+    ]);
+
+    birthDate = new NumerologyBirthDate(2, 2, 2002);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'procrastination',
+      'research',
+      'forgetfulness',
+      'chaos',
+      'dissatisfaction',
+      'passivity',
+    ]);
+
+    birthDate = new NumerologyBirthDate(26, 4, 1952);
+    natalChart = new NatalChart(birthDate);
+    expect(natalChart.getArrows()).toEqual([
+      'purposefulness',
+      'will',
+    ]);
+  });
 });
